8da5fac574bd81485c372b88b0dadaa4515493f6
Scott Geigel <scott.geigel@gmail.com>
2024-11-13 22:09:48 -0600

    initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+const char* X_Tile = 
+"     \\\   / \n\
+      | |  \n\
+     /   \\";
+
+const char* O_Tile = 
+"      /-\\\n\
+     |   |\n\
+      \\-/";
+
+int main () {
+	printf(X_Tile);
+	printf("\n");
+	printf(O_Tile);
+	printf("\n");
+}
